07155353714a96414e543af82f514bb2a4b4c78e
Endre Bakken Stovner <endrebak@biocore-home.novalocal>
2020-08-18 11:01:15 +0200

    0.0.43

--- epic2/src/reads_to_bins.pyx
+++ epic2/src/reads_to_bins.pyx
@@ -15,6 +15,7 @@ from libc.stdint cimport uint32_t, uint16_t
 
 from epic2.src.read_bam import read_bam
 from epic2.src.genome_info import sniff
+from epic2.src.remove_out_of_bounds_bins import remove_out_of_bounds_bins
 
 from cython.operator import dereference
 from libcpp.algorithm cimport sort as stdsort
@@ -34,48 +35,48 @@ cimport cython
 
 import numpy as np
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.initializedcheck(False)
-cpdef remove_out_of_bounds_bins(count_dict, chromsizes, bin_size):
+# @cython.boundscheck(False)
+# @cython.wraparound(False)
+# @cython.initializedcheck(False)
+# cpdef remove_out_of_bounds_bins(count_dict, chromsizes, bin_size):
 
-    # original code: get_bed_coords() in make_graph_file.py
+#     # original code: get_bed_coords() in make_graph_file.py
 
-    cdef:
-        # uint32_t[::1] bins
-        # uint16_t[::1] counts
-        uint32_t chromsize
-        size_t i
+#     cdef:
+#         # uint32_t[::1] bins
+#         # uint16_t[::1] counts
+#         uint32_t chromsize
+#         size_t i
 
 
-    for chromosome, (bins, counts) in count_dict.items():
-        if not chromosome in chromsizes:
-            print("Chromosome {} found in file, but not in the chromsizes dict for the genome.".format(chromosome), file=sys.stderr)
-            continue
+#     for chromosome, (bins, counts) in count_dict.items():
+#         if not chromosome in chromsizes:
+#             print("Chromosome {} found in file, but not in the chromsizes dict for the genome.".format(chromosome), file=sys.stderr)
+#             continue
 
-        chromsize = chromsizes[chromosome]
+#         chromsize = chromsizes[chromosome]
 
-        try:
-            i = len(bins) - 1
-            while (i >= 0 and (bins[i]) > chromsize):
-                # print("For {} bin {} is out of bounds ({})\n".format(chromosome, bins[i], chromsize))
-                # print("It has a count of ({})\n".format(counts[i]))
-                # 52280 - 51862 = 418
-                i -= 1
-        except OverflowError as e:
-            print("\nAdditional info:\n")
-            print("Chromosome:", chromosome)
-            print("Chromsize:", chromsize)
-            raise e
-        # sys.stderr.write(chromosome + "\n")
-        # sys.stderr.write("{} {}\n".format(bins[len(bins) - 1], chromsize))
+#         try:
+#             i = len(bins) - 1
+#             while (i >= 0 and (bins[i]) > chromsize):
+#                 # print("For {} bin {} is out of bounds ({})\n".format(chromosome, bins[i], chromsize))
+#                 # print("It has a count of ({})\n".format(counts[i]))
+#                 # 52280 - 51862 = 418
+#                 i -= 1
+#         except OverflowError as e:
+#             print("\nAdditional info:\n")
+#             print("Chromosome:", chromosome)
+#             print("Chromsize:", chromsize)
+#             raise e
+#         # sys.stderr.write(chromosome + "\n")
+#         # sys.stderr.write("{} {}\n".format(bins[len(bins) - 1], chromsize))
 
-        if i != len(bins) - 1:
-            # print("-----")
-            # print(chromosome)
-            # print("length before:", len(counts))
-            count_dict[chromosome] = bins[:i+1], counts[:i+1]
-            # print("length after:", len(counts[:i + 1]))
+#         if i != len(bins) - 1:
+#             # print("-----")
+#             # print(chromosome)
+#             # print("length before:", len(counts))
+#             count_dict[chromosome] = bins[:i+1], counts[:i+1]
+#             # print("length after:", len(counts[:i + 1]))
 
 
 
